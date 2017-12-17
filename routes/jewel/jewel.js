6171ffe114179b5fdcb5a243efd28d8cb0e95eb1
Dheeraj <dheeraj.pal@rubiconred.com>
2017-12-17 11:03:28 +0530

    added message api

--- routes/jewel/jewel.js
+++ routes/jewel/jewel.js
@@ -10,6 +10,12 @@ const nexmo = new Nexmo({
   apiKey: 'd930eae1',
   apiSecret: '4691606a0b4360a2'
 });
+// Twilio Credentials 
+var accountSid = 'ACe3605c1a18c2c187f147efe8e1382f37';
+var authToken = 'b7d7850a99f0f83bfd4de4269229c91b';
+
+//require the Twilio module and create a REST client 
+var client = require('twilio')(accountSid, authToken);
 
 //Set storage engine
 const storage = multer.diskStorage({
@@ -99,16 +105,28 @@ router.get('/api/jewel/get/update/:id', function (req, res) {
 
 //share jewel
 router.post('/api/jewel/share', function (req, res) {
+
   var message = req.body;
-  nexmo.message.sendSms(
-    'NEXMO', '91' + message.number, message.jewel, { type: 'unicode' }, (err, responseData) => {
-      if (err) {
-        throw err;
-      } else {
-        res.send(responseData);
-      }
-    }
-  );
+
+  client.messages.create({
+    to: "+91" + message.number,
+    from: "+18029921408",
+    body: message.jewel,
+  }, function (err, message) {
+    if (err)
+      res.send(err);
+    console.log(message.sid);
+    res.send(message.sid);
+  });
+  // nexmo.message.sendSms(
+  //   'NEXMO', '91' + message.number, message.jewel, { type: 'unicode' }, (err, responseData) => {
+  //     if (err) {
+  //       throw err;
+  //     } else {
+  //       res.send(responseData);
+  //     }
+  //   }
+  // );
 });
 
 
