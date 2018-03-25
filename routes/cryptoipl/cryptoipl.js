07e56a18719b57d5afb7e2b1e3806fd78260a2a0
Maloth Naresh <idofmalothnaresh@gmail.com>
2018-03-25 12:52:15 +0530

    Node mail sender

--- routes/cryptoipl/cryptoipl.js
+++ routes/cryptoipl/cryptoipl.js
@@ -5,6 +5,8 @@
 var express = require('express'), router = express.Router();
 var passport = require("passport");
 
+const nodemailer = require('nodemailer');
+
 /* Users APIs */
 router.get('/api/cryptoipl', function (req, res) {
   Cryptoipl.getAll(function (err, cryptoipl) {
@@ -41,6 +43,61 @@ router.post('/api/cryptoipl/:id/updateinfo', function( req, res ) {
       throw err;
     return res.status(200).json({ data: team });
   });
+});
+
+router.post('/api/cryptoipl/sendmail', function( req, res) {
+  console.log(req.body);
+  const obj = req.body;
+  for( var i=0; i < 2; i++) {
+    wallet = i == 0 ? obj.oldOwner : obj.newOwner;
+    Cryptoipl.getOne("0x584e20986ac344adeb007ecc9d901c31db154624", function (err, cryptoipl) {
+      if (err) {
+        throw err;
+      }
+      if( !cryptoipl ) {
+        return 
+      }
+      console.log("response : ", cryptoipl);
+      let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        port: 25,
+        secure: false,
+        auth: {
+            user: "idofmalothnaresh@gmail.com",
+            pass: "kishan028"
+        },
+        tls: {
+          rejectUnauthorized: false
+        }
+      });
+
+      var fromMsg = "Congratulations from Crypto IPL Team. Your team " + 
+        obj.name + " has been successfully sold for price " + obj.oldPrice;
+      var toMsg = "Congratulations from Crypto IPL Team. You successfully have aquired new team " + obj.name + " for price " + obj.oldPrice + 
+      ". You would get new price " + obj.newPrice + " if someone owns it.";
+
+      var message = i == 0 ? fromMsg : toMsg;
+      console.log("i value: ",i);
+
+      console.log("message: ", message);
+
+      let mailOptions = {
+          from: '"Maloth Naresh" <idofmalothnaresh@gmail.com>',
+          to: cryptoipl.email,
+          subject: 'Crypto IPL notice',
+          text: message
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+              return console.log(error);
+          }
+          console.log('Message sent!');
+          console.log('Info: ', info);
+      });
+    });
+  }
+
 });
 
 module.exports = router;
