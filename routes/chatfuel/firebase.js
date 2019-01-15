542f1653d9a47b472779f377e9756399ee7b4092
user <amit.android@boltt.com>
2019-01-16 01:39:41 +0530

    changed

--- routes/chatfuel/firebase.js
+++ routes/chatfuel/firebase.js
@@ -1,12 +1,7 @@
 // firebase
 const firebase = require("firebase");
 config = {
-  apiKey: "AIzaSyDB6n9kZ8s26pqLaRFwoTacR4xTTTCZZk4",
-  authDomain: "lakshmi-53afa.firebaseapp.com",
-  databaseURL: "https://lakshmi-53afa.firebaseio.com",
-  projectId: "lakshmi-53afa",
-  storageBucket: "lakshmi-53afa.appspot.com",
-  messagingSenderId: "1014878228303"
+
 };
 firebase.initializeApp(config);
 exports.firebase = function () {
